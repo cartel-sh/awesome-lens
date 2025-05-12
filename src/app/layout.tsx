cf66ce5b698190da8a5291dcf864c2c63b534bf8
kualta <contact@kualta.dev>
2025-05-12 14:15:20 +0400

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from 'next-themes';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +32,7 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body>
-        {children}
+          {children}
       </body>
     </html>
   );
